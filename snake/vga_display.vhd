59dadf1a55a84d0cc5a11fac8629f2de5a506b04
not-lina <62222117+not-lina@users.noreply.github.com>
2020-04-22 16:18:33 -0700

    lcl

--- snake/vga_display.vhd
+++ snake/vga_display.vhd
@@ -30,64 +30,67 @@ architecture Behavioral of VGA_display is
     constant WALL_X_R: integer := 4;
 
     -- border wall
-    constant WALL_THICKNESS = 3;
-    signal
-
+    constant WALL_THICKNESS: integer := 3;
+	 
     -- object output signals -- signal to indicate if the coord is within the object
     signal pixels_on, landscape_on: std_logic;
     signal pixels_rgb, landscape_rgb: std_logic_vector(7 downto 0);
+	 
+	 signal draw: boolean := False;
+	 signal ltest: boolean := False;
 
-    -- register telling the exact position on display on screen.
-    signal pos_x : integer range 0 to 1023 := 100;
-    signal pos_y : integer range 0 to 1023 := 80;
-    signal tile_x: integer range 0 to TILES_X := 0;
-    signal tile_y: integer range 0 to TILES_Y := 0;
-    signal sw_buf: std_logic_vector(7 downto 0) := sw;
+ -- Intermediate register telling the exact position on display on screen.
+	 signal tile_x: integer range 0 to TILES_X := 0;
+	 signal tile_y: integer range 0 to TILES_Y := 0;
+	 signal sw_buf: std_logic_vector(7 downto 0) := sw;
 begin
+	cursor: process(clock) begin 
+	   sw_buf <= sw;
+		tile_x <= hcounter / TILE_SIZE;
+		tile_y <= vcounter / TILE_SIZE;
+	end process;
 
-  sw_buf <= sw;
-  pos_x <= hcounter;
-  pos_y <= vcounter;
-  tile_x <= hcounter / TILE_SIZE;
-  tile_y <= vcounter / TILE_SIZE;
 
   -- On every positive edge of the clock counter condition is checked,
     video_output: process(clock) begin
         if rising_edge(clock) then
             -- background, switch controlled
+				 pixels_rgb <= sw_buf;
+				 landscape_rgb <= "00000011";
+				 
              if (
-                 (pos_x >= 1) and
-                 (pos_x < MAX_X) and
-                 (pos_y >= 1) and
-                 (pos_y < MAX_Y ))
-             then  pixels_on <= '1' ;
-             else  pixels_on <='0';
+                 (hcounter >= 1) and
+                 (hcounter < MAX_X) and
+                 (vcounter >= 1) and
+                 (vcounter < MAX_Y ))
+             then  pixels_on <= '1';
+             else  pixels_on <= '0';
              end if;
-             pixels_rgb <= sw_buf;
 
             -- outer border wall
-            if( pixesl_on and
-                (tile_x > WALL_THICKNESS) and
+            if( (tile_x > WALL_THICKNESS) and
                 (tile_x < (TILES_X-WALL_THICKNESS)) and
                 (tile_y > WALL_THICKNESS) and
                 (tile_y < (TILES_Y - WALL_THICKNESS))
-            )then landscape_on <= '0';
-            else landscape_on <= '1'
-            landscape_rgb <= "00000011";
+				 ) 
+				then landscape_on <= '0';
+            else landscape_on <= '1';
+				end if;
+            
 
             -- wall 1
-            if (pixels_on and (not landscape_on) and
-                ((tile_x > 20 ) and
+             if (((tile_x > 20 ) and
                 (tile_x < (20 + 2 - WALL_THICKNESS)) and
                 (tile_y > 30) and
-                (tile_y < (30 + 5 - WALL_THICKNESS)))
-            )then landscape_on <= '0';
-            else landscape_on <= '1'
+                (tile_y < (30 + 5 - WALL_THICKNESS))))
+			   then landscape_on <= '0';
+            else landscape_on <= '1';
+				end if;
 
         end if;
     end process;
 
-    process (pixels_on, landscape_on, pixels_rgb, landscape_rgb) begin
+    process (pixels_on, landscape_on, landscape_rgb, pixels_rgb) begin
         if (pixels_on = '0') then pixels <= "00000000"; -- blank
         else
             if (landscape_on = '1') then pixels <= landscape_rgb;
