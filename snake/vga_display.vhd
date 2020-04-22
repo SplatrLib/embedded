3471949c61d8c648751e2886e315470f7012278c
not-lina <62222117+not-lina@users.noreply.github.com>
2020-04-22 15:07:27 -0400

    added updated snake vga files

---
+++ snake/vga_display.vhd
@@ -0,0 +1,82 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity VGA_display is port
+ (
+  -- Assuming 50MHz clock.If the clock is reduced then it might give the unexpected output.
+  clock: in std_logic;
+  sw: in std_logic_vector(7 downto 0);
+  -- The counter tells whether the correct position on the screen is reached where the data is to be displayed.
+  hcounter: in integer range 0 to 1023;
+  vcounter: in integer range 0 to 1023;
+
+  -- Output the colour that should appear on the screen.
+  pixels : out std_logic_vector(7 downto 0)
+ ); end VGA_display;
+
+architecture Behavioral of VGA_display is
+	-- screen dimensions
+    constant MAX_X: integer := 640;
+    constant MAX_Y: integer := 480;
+	-- tile mesh 
+	 constant TILE_SIZE: integer := 8;
+	 constant TILES_X: integer := 80;
+	 constant TILES_Y: integer :=60;
+	 
+	  -- wall left and right boundary of wall (full height)
+    constant WALL_X_L: integer := 7;
+    constant WALL_X_R: integer := 10;
+	 -- object output signals -- new signal to indicate if
+    -- scan coord is within ball
+	 
+    signal pixels_on, wall_on: std_logic;
+    signal pixels_rgb, wall_rgb: std_logic_vector(7 downto 0);
+	 
+ -- Intermediate register telling the exact position on display on screen.
+    signal x : integer range 0 to 1023 := 100;
+    signal y : integer range 0 to 1023 := 80;
+	 signal tile_x: integer range 0 to TILES_X := 0;
+	 signal tile_y: integer range 0 to TILES_Y := 0;
+	 signal sw_buf: std_logic_vector(7 downto 0) := sw;
+begin
+
+  sw_buf <= sw;
+  x <= hcounter;
+  y <= vcounter;
+  -- On every positive edge of the clock counter condition is checked,
+  
+  video_output: process(clock) begin
+	if rising_edge(clock) then
+	
+		-- background, switch controlled
+		
+		 if ( 
+			 (x >= 1) and 
+			 (x < 480) and 
+			 (y >= 1) and 
+			 (y < 640 ))
+		 then  pixels_on <= '1' ;
+		 else pixels_on <='0';
+		 end if;
+		 pixels_rgb <= sw_buf;
+	 
+		-- wall left vertical stripe
+		 if( 
+		  (WALL_X_L <= x) and(x <= WALL_X_R))
+	   then wall_on <= '1';
+		else wall_on <= '0';
+		end if;
+		wall_rgb <= "00000011"; -- blue
+   end if;
+  end process;
+  
+	process (pixels_on, wall_on, pixels_rgb, wall_rgb) begin
+		if (pixels_on = '0') then pixels <= "00000000"; -- blank
+		else
+			if (wall_on = '1') then pixels <= wall_rgb;
+			else pixels <= pixels_rgb;
+			end if;
+		end if;
+	end process;
+ end Behavioral;
---
+++ snake/vga_top.vhd
@@ -0,0 +1,65 @@
+
+--/* https://langster1980.blogspot.com/2015/08/driving-vga-port-using-elbert-v2-and_7.html */
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity VGA_top_module is
+    port (
+        --/* Input: the 12MHz input clock */
+        Clk : IN STD_LOGIC;
+		  sw : in std_logic_vector(7 downto 0);
+        --/* Output: ElbertV2 VGA Display */
+        hsync : OUT STD_LOGIC;
+        vsync : OUT STD_LOGIC;
+        rgb   : OUT STD_LOGIC_VECTOR(7 downto 0)
+    );
+
+end VGA_top_module;
+
+architecture Behavioral of VGA_top_module is
+
+    component clocking_inst
+        port(
+            --/*Input clock of 12MHz */
+            CLKIN_IN         : in     std_logic;
+
+            --/* Output clock has to been set to 100 MHz.
+            --If input clock frequency changes then update clocking_inst to reflect the change */
+            CLKFX_OUT          : out    std_logic
+        );
+    end component;
+
+    component VGA_sync
+        port    (
+            -- Input Clock 100MHz
+            Clk   : in std_logic;
+				sw: in std_logic_vector(7 downto 0);
+            -- Output for the VGA Display
+            hsync    : out std_logic;
+            vsync    : out std_logic;
+            rgb      : out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+    signal  clock   : std_logic := '0';
+
+begin 
+
+    clocking: clocking_inst
+    port map (
+        CLKIN_IN        => Clk,
+        CLKFX_OUT       => clock
+    );
+
+    VGA_inst: VGA_sync
+    port map (
+        Clk   => clock,
+		  sw => sw,
+        hsync => hsync,
+        vsync => vsync,
+        rgb => rgb
+    );
+
+end architecture Behavioral;
